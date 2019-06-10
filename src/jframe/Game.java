fb7baf568d20b1916925166b3d0b10cff7eb9281
Robert Chen <robertchen@live.com>
2019-06-10 13:00:12 -0700

    Uploaded jframe example

---
+++ src/jframe/Game.java
@@ -0,0 +1,12 @@
+package jframe;
+
+import javax.swing.JPanel;
+
+/*
+ * We'll do more with this later. 
+ * 
+ * When we write "Game extends JPanel", it means the class "Game" is a "JPanel". 
+ */
+public class Game extends JPanel {
+
+}
---
+++ src/jframe/Main.java
@@ -0,0 +1,34 @@
+package jframe;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+	public static void main(String args[]) {
+		/*
+		 * JFrame is the actual window that popups.
+		 */
+		JFrame frame = new JFrame("Frame Title");
+
+		/*
+		 * The content that is actually displayed is represented through JPanels. In
+		 * order to create our own animations, we'll need to have a custom JPanel. We
+		 * use frame.add() to add a JPanel to the window.
+		 */
+		JPanel game = new Game();
+		frame.add(game);
+
+		/*
+		 * Use frame.setPreferredSize to set the size of the window.
+		 */
+		frame.setPreferredSize(new Dimension(500, 500));
+		frame.pack();
+
+		/*
+		 * Makes the frame visible (it's invisible by default).
+		 */
+		frame.setVisible(true);
+	}
+}
